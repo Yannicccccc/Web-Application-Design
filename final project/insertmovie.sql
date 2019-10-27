5063edaa5981d30225b7be154813b51e252ceff0
Luo Jingying <m18344316380@gmail.com>
2019-10-27 15:12:33 +0800

    moviedetail_v2

--- final project/insertmovie.sql
+++ final project/insertmovie.sql
@@ -1,3 +1,19 @@
 INSERT INTO movie VALUES
     (1, "A Witness Out Of Blue", "NC16", 4.5, "Louis Koo, Jessica Hsuan , Louis Cheung, Philip Keung , Fiona Sit", "English & Chinese", "Fung Chih Chiang", "Chinese", "2019-10-17"),
-    (3, "Maleficient: Mistress Of Evil", 4.8, "Angelina Jolie, Elle Fanning, Chiwetel Ejiofor, Sam Riley, Imelda Staunton, Juno Temple, Lesley Manville", "Chinese", "Joachim Rønning", )
+    (2, "Bigil", "PG13", 4.2, "Vijay, Nayanthara, Jackie Sharoff, Vivek, Yogi Babu, Kathir", "English", "Atlee", "Tamil", "2019-10-25"),
+    (3, "Maleficient: Mistress Of Evil", "PG", 4.8, "Angelina Jolie, Elle Fanning, Chiwetel Ejiofor, Sam Riley, Imelda Staunton, Juno Temple, Lesley Manville", "Chinese", "Joachim Rønning", "2019-10-17"),
+    (4, "Terminator: Dark Fate", "NC16", 4.9, "Linda Hamilton, Arnold Schwarzenegger, Mackenzie Davis, Natalia Reyes, Gabriel Luna, Diego Boneta", "Chinese", "Tim Miller", "English", "2019-10-24"),
+    (5, "The Kill Team", "NC16", 4.6, "Alexander Skarsgård, Nat Wolff, Adam Long, Jonathan Whitesell, Brian Marc, Rob Morrow", "N.A.", "Dan Krauss", "English", "2019-10-24");
+
+INSERT INTO movie_genre VALUES
+    (1, 1, "Crime"),
+    (2, 2, "Action"),
+    (3, 2, "Sport"),
+    (4, 3, "Adventure"),
+    (5, 3, "Family"),
+    (6, 3, "Fantasy"),
+    (7, 4, "Action"),
+    (8, 4, "Adventure"),
+    (9, 5, "Action"),
+    (10, 5, "Drama"),
+    (11, 5, "Thriller");    
