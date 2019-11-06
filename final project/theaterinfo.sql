ead3c3e340578fd1714d094f7a6d1e5b14b12ce0
Luo Jingying <m18344316380@gmail.com>
2019-11-07 00:24:44 +0800

    booking

---
+++ final project/theaterinfo.sql
@@ -0,0 +1,20 @@
+CREATE TABLE theater (
+    theaterid INT NOT NULL PRIMARY KEY,
+    theatername VARCHAR(10),
+    theaterdescription VARCHAR(500),
+    activate INT
+);
+
+CREATE TABLE theatermovie (
+    movieid INT NOT NULL PRIMARY KEY,
+    theaterid INT NOT NULL REFERENCES theater(theaterid),
+    title VARCHAR(50)
+);
+
+CREATE TABLE theaterslot (
+    slotid INT NOT NULL PRIMARY KEY,
+    movieid INT NOT NULL REFERENCES theatermovie(movieid),
+    slotdate VARCHAR(10),
+    slot VARCHAR(10),
+    seat VARCHAR(50)
+);
